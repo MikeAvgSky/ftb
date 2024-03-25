6d36cb8771186aadc5832b3d417f537bfd314d50
Mike Avgeros <mike.avgeros@sky.uk>
2024-03-25 13:27:06 +0000

    Remove unused code

--- src/Trading.Bot/Configuration/TradeConfiguration.cs
+++ src/Trading.Bot/Configuration/TradeConfiguration.cs
@@ -4,7 +4,6 @@ public class TradeConfiguration
 {
     public bool RunBot { get; set; }
     public bool StopRollover { get; set; }
-    public bool CheckCandleContinuity { get; set; }
     public int TradeRisk { get; set; }
     public TradeSettings[] TradeSettings { get; set; }
 }
