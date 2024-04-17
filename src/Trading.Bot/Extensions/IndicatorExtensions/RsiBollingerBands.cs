ba79a568650affa37f823dc54a1a56ccf7d19bd6
Mike Avgeros <mike.avgeros@sky.uk>
2024-04-17 18:40:39 +0100

    Update RsiBollingerBands indicator

--- src/Trading.Bot/Extensions/IndicatorExtensions/RsiBollingerBands.cs
+++ src/Trading.Bot/Extensions/IndicatorExtensions/RsiBollingerBands.cs
@@ -3,8 +3,7 @@
 public static partial class Indicator
 {
     public static IndicatorResult[] CalcRsiBollingerBands(this Candle[] candles, int bbWindow = 20, int rsiWindow = 14, double stdDev = 2,
-        double maxSpread = 0.0004, double minGain = 0.0006, int minVolume = 100, double riskReward = 1.5, double rsiLower = 30, double rsiUpper = 70,
-        bool breakout = false)
+        double maxSpread = 0.0004, double minGain = 0.0006, int minVolume = 100, double riskReward = 1.5, double rsiLower = 30, double rsiUpper = 70)
     {
         var rsiResults = candles.CalcRsi(rsiWindow);
 
@@ -20,36 +19,22 @@ public static partial class Indicator
 
             result[i].Candle = candles[i];
 
-            result[i].Gain = Math.Abs(candles[i].Mid_C - bollingerBands[i].Sma);
+            result[i].Gain = Math.Abs(candles[i].Mid_O - bollingerBands[i].Sma);
 
             result[i].Signal = i == 0 ? Signal.None : candles[i] switch
             {
-                var candle when candle.Mid_C < bollingerBands[i].LowerBand &&
-                                candle.Mid_O > bollingerBands[i].LowerBand &&
-                                rsiResults[i].Rsi < rsiLower &&
+                var candle when candle.Mid_C > bollingerBands[i].LowerBand &&
+                                candles[i - 1].Mid_C < bollingerBands[i - 1].LowerBand &&
+                                rsiResults[i - 1].Rsi < rsiLower &&
                                 candle.Spread <= maxSpread &&
                                 candle.Volume >= minVolume &&
-                                result[i].Gain >= minGain && !breakout => Signal.Buy,
-                var candle when candle.Mid_C > bollingerBands[i].UpperBand &&
-                                candle.Mid_O < bollingerBands[i].UpperBand &&
-                                rsiResults[i].Rsi > rsiUpper &&
-                                rsiResults[i].Rsi > rsiResults[i - 1].Rsi &&
+                                result[i].Gain >= minGain => Signal.Buy,
+                var candle when candle.Mid_C < bollingerBands[i].UpperBand &&
+                                candles[i - 1].Mid_C > bollingerBands[i - 1].UpperBand &&
+                                rsiResults[i - 1].Rsi > rsiUpper &&
                                 candle.Spread <= maxSpread &&
                                 candle.Volume >= minVolume &&
-                                result[i].Gain >= minGain && breakout => Signal.Buy,
-                var candle when candle.Mid_C > bollingerBands[i].UpperBand &&
-                                candle.Mid_O < bollingerBands[i].UpperBand &&
-                                rsiResults[i].Rsi > rsiUpper &&
-                                candle.Spread <= maxSpread &&
-                                candle.Volume >= minVolume &&
-                                result[i].Gain >= minGain && !breakout => Signal.Sell,
-                var candle when candle.Mid_C < bollingerBands[i].LowerBand &&
-                                candle.Mid_O > bollingerBands[i].LowerBand &&
-                                rsiResults[i].Rsi < rsiLower &&
-                                rsiResults[i].Rsi < rsiResults[i - 1].Rsi &&
-                                candle.Spread <= maxSpread &&
-                                candle.Volume >= minVolume &&
-                                result[i].Gain >= minGain && breakout => Signal.Sell,
+                                result[i].Gain >= minGain => Signal.Sell,
                 _ => Signal.None
             };
 
