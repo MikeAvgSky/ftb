e7207d285f86f986e9f941dd0d0ffce3bec197fd
Mike Avgeros <mike.avgeros@sky.uk>
2024-05-09 20:24:01 +0100

    Update indicator

--- src/Trading.Bot/Extensions/IndicatorExtensions/BollingerBandsEma.cs
+++ src/Trading.Bot/Extensions/IndicatorExtensions/BollingerBandsEma.cs
@@ -25,15 +25,15 @@ public static partial class Indicator
 
             result[i].Signal = i == 0 ? Signal.None : candles[i] switch
             {
-                var candle when candle.Mid_O > bollingerBands[i].LowerBand &&
-                                candle.Mid_C < bollingerBands[i].LowerBand &&
-                                candle.Mid_L > emaResult[i] &&
+                var candle when candle.Mid_O < bollingerBands[i].Sma &&
+                                candle.Mid_C > bollingerBands[i].Sma &&
+                                emaResult[i] < bollingerBands[i].LowerBand &&
                                 candle.Spread <= maxSpread &&
                                 candle.Volume >= minVolume &&
                                 result[i].Gain >= minGain => Signal.Buy,
-                var candle when candle.Mid_O < bollingerBands[i].UpperBand &&
-                                candle.Mid_C > bollingerBands[i].UpperBand &&
-                                candle.Mid_H < emaResult[i] &&
+                var candle when candle.Mid_O > bollingerBands[i].Sma &&
+                                candle.Mid_C < bollingerBands[i].Sma &&
+                                emaResult[i] > bollingerBands[i].UpperBand &&
                                 candle.Spread <= maxSpread &&
                                 candle.Volume >= minVolume &&
                                 result[i].Gain >= minGain => Signal.Sell,
